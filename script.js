197bf24807d18e598caf8d8dd3282f92389f2b98
Joseph Martin <se.josephmartin@gmail.com>
2023-06-06 22:05:26 +0800

    target element selector in js

---
+++ script.js
@@ -0,0 +1,9 @@
+const operatorBtns = document.querySelectorAll(".operatorKey");
+const numberBtns = document.querySelectorAll(".numberKey");
+const allClear = document.querySelector("#all-clear");
+const deleteBtn = document.querySelector("#backspace");
+const decimal = document.querySelector("#point");
+const equals = document.querySelector("#equals");
+
+
+
